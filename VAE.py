ddecf125ad2976c75813150a8c4747c9406ca00f
Steven Bamford <steven@stevenbamford.com>
2021-02-16 01:26:18 +0000

    Working basic denoising CVAE on MNIST data
    This is the simplest way I could come up with for implementing
    a CVAE with different input and 'target' images.
    
    I'm really not a fan of the current keras example, it seems
    to add unnecessary complexity.
    
    It does ok, but note that a small CVAE isn't particularly well-suited
    to producing good reconstructions of MNIST digits. You can get
    improvements by increasing the number of latent
    dimensions and/or lowering `beta` (which reduces the 'smoothness'
    enforced on the latent space). It is also important to let it train for
    long enough (until the validation loss stops improving). This can take a
    while, especially on a CPU.

--- VAE.py
+++ VAE.py
@@ -3,19 +3,39 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 import matplotlib.pyplot as plt
-from keras.callbacks import TensorBoard
+from tensorflow.keras.callbacks import TensorBoard
+from datetime import datetime
+
+# Creates noisy digits
+
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+#x_train = x_train[y_train == 5]
+#x_test = x_test[y_test == 5]
+
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))
+x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))
+
+noise_factor = 0.5
+x_train_noisy = x_train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_train.shape) 
+x_test_noisy = x_test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_test.shape) 
+
+x_train_noisy = np.clip(x_train_noisy, 0., 1.)
+x_test_noisy = np.clip(x_test_noisy, 0., 1.)
 
 # create a sampling layer
 
 class Sampling(layers.Layer):
-"""Uses (z_mean, z_log_var) to sample z, the vector encoding a digit."""
+    """Uses (z_mean, z_log_var) to sample z, the vector encoding a digit."""
 
-def call(self, inputs):
-    z_mean, z_log_var = inputs
-    batch = tf.shape(z_mean)[0]
-    dim = tf.shape(z_mean)[1]
-    epsilon = tf.keras.backend.random_normal(shape=(batch, dim))
-    return z_mean + tf.exp(0.5 * z_log_var) * epsilon
+    def call(self, inputs):
+        z_mean, z_log_var = inputs
+        batch = tf.shape(z_mean)[0]
+        dim = tf.shape(z_mean)[1]
+        epsilon = tf.keras.backend.random_normal(shape=(batch, dim))
+        return z_mean + tf.exp(0.5 * z_log_var) * epsilon
     
 # build the encoder
 
@@ -43,86 +63,49 @@ decoder_outputs = layers.Conv2DTranspose(1, 3, activation="sigmoid", padding="sa
 decoder = keras.Model(latent_inputs, decoder_outputs, name="decoder")
 decoder.summary()
 
-# define the VAE as a model with custom train step
+# define the VAE model
 
-# NEED TO EDIT CLASS
+def reconstruction_loss(targets, outputs):
+    loss = keras.losses.mean_squared_error(outputs, targets)
+    loss = tf.reduce_mean(tf.reduce_sum(loss, axis=(1, 2)))
+    return loss
 
-class VAE(keras.Model):
-    def __init__(self, encoder, decoder, **kwargs):
-        super(VAE, self).__init__(**kwargs)
-        self.encoder = encoder
-        self.decoder = decoder
-        self.total_loss_tracker = keras.metrics.Mean(name="total_loss")
-        self.reconstruction_loss_tracker = keras.metrics.Mean(
-            name="reconstruction_loss"
-        )
-        self.kl_loss_tracker = keras.metrics.Mean(name="kl_loss")
+beta = 1.0
+kl_loss = -0.5 * (1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
+kl_loss = beta * tf.reduce_mean(tf.reduce_sum(kl_loss, axis=1))
 
-    @property
-    def metrics(self):
-        return [
-            self.total_loss_tracker,
-            self.reconstruction_loss_tracker,
-            self.kl_loss_tracker,
-        ]
+vae_outputs = decoder(encoder(encoder_inputs)[-1])
+vae = keras.Model(encoder_inputs, vae_outputs)
+vae.add_loss(kl_loss)
+vae.add_metric(kl_loss, name='kl_loss')
 
-    def train_step(self, data):
-        with tf.GradientTape() as tape:
-            z_mean, z_log_var, z = self.encoder(data)
-            reconstruction = self.decoder(z)
-            reconstruction_loss = tf.reduce_mean(
-                tf.reduce_sum(
-                    keras.losses.binary_crossentropy(data, reconstruction), axis=(1, 2)
-                )
-            )
-            kl_loss = -0.5 * (1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
-            kl_loss = tf.reduce_mean(tf.reduce_sum(kl_loss, axis=1))
-            total_loss = reconstruction_loss + kl_loss
-        grads = tape.gradient(total_loss, self.trainable_weights)
-        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
-        self.total_loss_tracker.update_state(total_loss)
-        self.reconstruction_loss_tracker.update_state(reconstruction_loss)
-        self.kl_loss_tracker.update_state(kl_loss)
-        return {
-            "loss": self.total_loss_tracker.result(),
-            "reconstruction_loss": self.reconstruction_loss_tracker.result(),
-            "kl_loss": self.kl_loss_tracker.result(),
-        }
-
-# Creates noisy digits
-
-(x_train, _), (x_test, _) = keras.datasets.mnist.load_data()
-x_train = x_train.astype('float32') / 255.
-x_test = x_test.astype('float32') / 255.
-x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))
-x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))
-
-noise_factor = 0.5
-x_train_noisy = x_train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_train.shape) 
-x_test_noisy = x_test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_test.shape) 
-
-x_train_noisy = np.clip(x_train_noisy, 0., 1.)
-x_test_noisy = np.clip(x_test_noisy, 0., 1.)
-
-# shows what the noisy digits look like
-
-n = 10
-plt.figure(figsize=(20, 2))
-for i in range(1, n + 1):
-    ax = plt.subplot(1, n, i)
-    plt.imshow(x_test_noisy[i].reshape(28, 28))
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-    plt.show()
+vae.compile(optimizer=keras.optimizers.Adam(), loss=reconstruction_loss,
+            metrics=[reconstruction_loss])
 
 # train the VAE
 
-vae = VAE(encoder, decoder)
-vae.compile(optimizer=keras.optimizers.Adam())
+logdir = "/tmp/tb/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = keras.callbacks.TensorBoard(log_dir=logdir)
+
 vae.fit(x_train_noisy, x_train,
-                epochs=10,
-                batch_size=128,
-                shuffle=True,
-                validation_data=(x_test_noisy, x_test),
-                callbacks=[TensorBoard(log_dir='/tmp/tb', histogram_freq=0, write_graph=False)])
+        epochs=60,
+        batch_size=128,
+        shuffle=True,
+        validation_data=(x_test_noisy, x_test),
+        callbacks=[tensorboard_callback])
+
+# show what the original, noisy and reconstructed digits look like
+
+reconstructions = vae.predict(x_test_noisy)
+
+n = 10
+fig, axarr = plt.subplots(3, n, figsize=(20, 6))
+for i, ax in enumerate(axarr[0]):
+    ax.imshow(x_test[i], cmap='gray')
+for i, ax in enumerate(axarr[1]):
+    ax.imshow(x_test_noisy[i], cmap='gray')
+for i, ax in enumerate(axarr[2]):
+    ax.imshow(reconstructions[i], cmap='gray')
+for ax in axarr.flat:
+    ax.axis('off')
+plt.savefig('examples.pdf')
