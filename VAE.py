761ba0c9958b09312360db7605428574fa2f9c89
llpalethorpe <78367556+llpalethorpe@users.noreply.github.com>
2021-02-21 12:34:00 +0000

    Update VAE.py

--- VAE.py
+++ VAE.py
@@ -3,7 +3,6 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 import matplotlib.pyplot as plt
-from tensorflow.keras.callbacks import TensorBoard
 from datetime import datetime
 
 # Creates noisy digits
