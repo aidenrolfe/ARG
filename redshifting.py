11dab5fec873d7cc068314c567612c4347de8596
Elizabeth <74026161+Lizzylooie@users.noreply.github.com>
2021-03-15 15:36:28 +0000

    Update redshifting.py

--- redshifting.py
+++ redshifting.py
@@ -76,15 +76,16 @@ def rebinning(image, input_redshift, output_redshift):
   # does this need to be normalised by dividing by the sum of the image/flux?
     
 # adding shot noise (from variations in the detection of photons from the source):
-def add_shot_noise(image, output_exptime):            
-    # shot_noise = np.sqrt(convolved * output_exptime) * np.random.poisson()
+def add_shot_noise(image, output_exptime):
+    
     with_shot_noise = np.random.poisson(image)
     return with_shot_noise
 
 # adding background noise (from numerous sources - i.e. the sky, electrons in detector which are appearing randomly from thermal noise): 
 def add_background(image):
-    # code to generate background noise, background = ...
-    with_background = np.random.normal(mean=0, peak, image)
+    
+    # peak needs to be tested to find a suitable value to determine the amount of background noise added to be realistic
+    with_background = np.random.normal(mean=0, peak=1, image=image)
     return with_background
 
 # saving data for use in VAE:
