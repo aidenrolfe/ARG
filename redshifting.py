e893fa9c33633c5bfef367c419523252315bd8c3
aidenrolfe <aidenrolfe@pm.me>
2021-03-15 22:55:36 +0000

    added image dimming

--- redshifting.py
+++ redshifting.py
@@ -2,9 +2,12 @@ import numpy as np
 from matplotlib import pyplot as plt
 from scipy.ndimage import zoom
 from astropy import convolution as conv
-from astropy import cosmology as cosmo
-import cv2
+from astropy.cosmology import FlatLambdaCDM
+import astropy.units as u
+from scipy import signal as sig
+import random
 
+cosmo = FlatLambdaCDM(H0=70 * u.km / u.s / u.Mpc, Tcmb0=2.725 * u.K, Om0=0.3)
 
 # read in galaxy:
 with open('inputgalaxies.npy','rb') as f:
@@ -15,32 +18,67 @@ with open('targetgalaxies.npy','rb') as g:
     gal_target = np.load(g)
     output_redshift = np.load(g)
 
-image = gal_input[0,...] # testing with first input image
+n = gal_input.shape[0]
+m = random.randint(0,n-1)
 
-image = cv2.resize(image,(81,81)) # resize image to fit kernel
+image = gal_input[m,...] # testing with one input image (all filters)
 
-# # observing simulated galaxy:
+sd = 1.5 # std dev of gaussian
+seeing = 2.354*sd # FWHM ~ 2.354*sd
+
+# observing simulated galaxy:
+# resize object to fit kernel
 # def observe_gal(image, input_redshift, output_redshift, seeing):
 #     image = resize(image, input_redshift, output_redshift)
 #     return image
 
-seeing = 10 # just for testing
 
-# convolving with a psf:
+# convolving image with gaussian psf:
 def convolve_psf(image, seeing):
-    psf = conv.Gaussian2DKernel(seeing)
-    convolved = np.convolve(image[...,0], psf)
+    psf = conv.Gaussian2DKernel(seeing, x_size=60, y_size=60)
+    plt.imshow(psf.array, interpolation='none', origin='lower')
+    convolved = np.empty((17, 60, 60))
+    # convolve PSF with image in all 17 filters
+    for i in range(17):
+        convolved[i] = sig.convolve2d(image[...,i], psf.array, mode = 'same')
     return convolved
 
-convolved_image = convolve_psf(image[...,0],seeing)
+convolved_image = convolve_psf(image,seeing)
+
+# plot image before PSF in all filteres
+for j in range(17):
+    plt.subplot(3,6,j+1)
+    plt.imshow(image[...,j], interpolation='none', origin='lower', cmap='inferno', vmin=0, vmax=1)
+    plt.axis('off') 
+plt.suptitle('Image ' + str(m) + ' at redshift = ' + str(input_redshift) + ' before PSF') 
+plt.show  
+
+# plot image after PSF in all filters
+for k in range(17):
+    plt.subplot(3,6,k+1)
+    plt.imshow(convolved_image[k], interpolation='none', origin='lower', cmap='inferno', vmin=0, vmax=1)
+    plt.axis('off') 
+plt.suptitle('Image ' + str(m) + ' at redshift = ' + str(input_redshift) + ' after PSF') 
+plt.show()   
+
 
 # changes to brightness:
 def dimming(image, input_redshift, output_redshift):
     d_i = cosmo.luminosity_distance(input_redshift)
-    d_o = cosmo.luminsoity_distance(output_redshift)
-    dimming = (d_i / d_o)**2
-    dimmed = image * dimming
+    d_o = cosmo.luminosity_distance(output_redshift)
+    dimming = (d_i / d_o)
+    dimmed = convolved_image*dimming
     return dimmed
+
+dimmed = dimming(convolved_image, input_redshift, output_redshift)
+
+# plot dimmed image in all filters
+for l in range(17):
+    plt.subplot(3,6,l+1)
+    plt.imshow(dimmed[l], interpolation='none', origin='lower', cmap='inferno', vmin=0, vmax=1)
+    plt.axis('off') 
+plt.suptitle('Image ' + str(m) + ' at redshift = ' + str(input_redshift) + ' after PSF and dimming') 
+plt.show()  
 
 # changes to size:
 def rebinning(image, input_redshift, output_redshift):
@@ -55,13 +93,13 @@ def rebinning(image, input_redshift, output_redshift):
 # adding shot noise (from variations in the detection of photons from the source):
 def add_shot_noise(image, output_exptime):            
     # shot_noise = np.sqrt(convolved * output_exptime) * np.random.poisson()
-    with_shot_noise = image + shot_noise
+    with_shot_noise = np.random.poisson(image)
     return with_shot_noise
 
 # adding background noise (from numerous sources - i.e. the sky, electrons in detector which are appearing randomly from thermal noise): 
 def add_background(image):
     # code to generate background noise, background = ...
-    with_background = image + background
+    with_background = np.random.normal(...)
     return with_background
 
 # saving data for use in VAE:
