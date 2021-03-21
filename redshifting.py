086f6a70f526e7d534e8ba2af9d1cec545e482ae
aidenrolfe <aidenrolfe@pm.me>
2021-03-21 20:45:11 +0000

    PSF convolved with all input and target images + saves result to file

--- redshifting.py
+++ redshifting.py
@@ -1,84 +1,52 @@
 import numpy as np
-from matplotlib import pyplot as plt
 from scipy.ndimage import zoom
-from astropy import convolution as conv
+from astropy.convolution import convolve_fft
+import astropy.convolution as conv
 from astropy.cosmology import FlatLambdaCDM
 import astropy.units as u
-from scipy import signal as sig
-import random
 
 cosmo = FlatLambdaCDM(H0=70 * u.km / u.s / u.Mpc, Tcmb0=2.725 * u.K, Om0=0.3)
 
-# read in galaxy:
-with open('inputgalaxies.npy','rb') as f:
-    gal_input = np.load(f)
-    input_redshift = np.load(f)
-        
-with open('targetgalaxies.npy','rb') as g:
-    gal_target = np.load(g)
-    output_redshift = np.load(g)
+# read in galaxy: 
+image = np.load('inputgalaxies.npy')
+input_redshift = np.load('inputredshifts.npy')    
+target_image = np.load('targetgalaxies.npy')
+output_redshift = np.load('targetredshifts.npy')
 
-n = gal_input.shape[0]
-m = random.randint(0,n-1)
-
-image = gal_input[m,...] # testing with one input image (all filters)
+n = image.shape[0]
 
 sd = 1.5 # std dev of gaussian
 seeing = 2.354*sd # FWHM ~ 2.354*sd
 
-# observing simulated galaxy:
-# resize object to fit kernel
-# def observe_gal(image, input_redshift, output_redshift, seeing):
-#     image = resize(image, input_redshift, output_redshift)
-#     return image
 
-
-# convolving image with gaussian psf:
+# convolving image with gaussian psf
 def convolve_psf(image, seeing):
     psf = conv.Gaussian2DKernel(seeing, x_size=60, y_size=60)
-    plt.imshow(psf.array, interpolation='none', origin='lower')
-    convolved = np.empty((17, 60, 60))
-    # convolve PSF with image in all 17 filters
-    for i in range(17):
-        convolved[i] = sig.convolve2d(image[...,i], psf.array, mode = 'same')
-    return convolved
+    convolved = np.empty((100, 60, 60, 17))
+    target_convolved = np.empty((100, 60, 60, 17))
+    # convolve PSF with images in all 17 filters
+    for h in range(n):
+        for i in range(17):
+            convolved[h,...,i] = convolve_fft(image[h,...,i], psf.array)
+            target_convolved[h,...,i] = convolve_fft(target_image[h,...,i], psf.array)
+    return convolved, target_convolved
 
-convolved_image = convolve_psf(image,seeing)
+convolved_image, target_convolved_image = convolve_psf(image,seeing)
 
-# plot image before PSF in all filteres
-for j in range(17):
-    plt.subplot(3,6,j+1)
-    plt.imshow(image[...,j], interpolation='none', origin='lower', cmap='inferno', vmin=0, vmax=1)
-    plt.axis('off') 
-plt.suptitle('Image ' + str(m) + ' at redshift = ' + str(input_redshift) + ' before PSF') 
-plt.show  
-
-# plot image after PSF in all filters
-for k in range(17):
-    plt.subplot(3,6,k+1)
-    plt.imshow(convolved_image[k], interpolation='none', origin='lower', cmap='inferno', vmin=0, vmax=1)
-    plt.axis('off') 
-plt.suptitle('Image ' + str(m) + ' at redshift = ' + str(input_redshift) + ' after PSF') 
-plt.show()   
-
-
-# changes to brightness:
+# changes to brightness (probably not keeping)
 def dimming(image, input_redshift, output_redshift):
-    d_i = cosmo.luminosity_distance(input_redshift)
-    d_o = cosmo.luminosity_distance(output_redshift)
+    d_i = cosmo.luminosity_distance(input_redshift[0])
+    d_o = cosmo.luminosity_distance(output_redshift[0])
     dimming = (d_i / d_o)
     dimmed = convolved_image*dimming
     return dimmed
 
 dimmed = dimming(convolved_image, input_redshift, output_redshift)
 
-# plot dimmed image in all filters
-for l in range(17):
-    plt.subplot(3,6,l+1)
-    plt.imshow(dimmed[l], interpolation='none', origin='lower', cmap='inferno', vmin=0, vmax=1)
-    plt.axis('off') 
-plt.suptitle('Image ' + str(m) + ' at redshift = ' + str(input_redshift) + ' after PSF and dimming') 
-plt.show()  
+# saving convolved input and target image
+np.save('convolvedimage.npy', convolved_image)
+np.save('convolvedtargetimage.npy', target_convolved_image)
+
 
 # changes to size:
 def rebinning(image, input_redshift, output_redshift):
